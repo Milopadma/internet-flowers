046e95b6a34690751330e3fa50825894f257c4d7
mylo <milopadma@yahoo.com>
2024-05-11 02:23:49 +0800

    interesting

---
+++ supabase-functions/supabase/functions/new-user/index.ts
@@ -0,0 +1,62 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
+};
+
+Deno.serve(async (req) => {
+  try {
+    const supabase = createClient(
+      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      {
+        global: {
+          headers: { Authorization: req.headers.get("Authorization")! },
+        },
+      },
+    );
+
+    console.log(supabase);
+
+    // And we can run queries in the context of our authenticated user
+    const { data, error } = await supabase.from("user").select("*");
+    console.log("dbconn: ", data, error);
+    if (error) throw error;
+    let msg = {};
+    {
+      const { data } = await req.json();
+      const { email_addresses } = data;
+      const email = email_addresses[0].email_address;
+      console.log(email.split("@")[0]);
+
+      msg = {
+        email_addresses: email_addresses,
+        email: email,
+        name: email.split("@")[0],
+      };
+    }
+
+    return new Response(JSON.stringify({ data, msg }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/new-user' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
